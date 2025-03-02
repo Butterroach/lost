852d34c01ab5f03964b92a4b17c0b12870e6fae7
Butterroach <butterroach@outlook.com>
2025-03-02 22:01:28 +0300

    v1.0.0sudo .venv/bin/python3 app.pysudo .venv/bin/python3 app.pysudo .venv/bin/python3 app.py

---
+++ README.md
@@ -0,0 +1,52 @@
+<h1 align="center">Lost</h1>
+
+Lost is a lightweight Linux hosts manager made in Python and QT6.
+
+## Features
+
+-   You can add sources
+-   You can update your sources
+-   You can remove your sources
+-   ...that's pretty much all you need (no it doesn't include auto updating that's stupid!!!
+    you shouldn't leave updates of stuff like that unattended!!! who wants broken internet cuz that one program you installed 2 weeks ago went rogue????)
+
+## How to install
+
+1. Download the source code of the [latest release](https://github.com/Butterroach/lost/releases/latest). Do NOT download the repo as a zip or git clone.
+2. Extract the archive into a directory.
+3. Go into the directory with a terminal.
+4. Create a venv: `python3 -m venv .venv`.
+5. Activate the venv: `source .venv/bin/activate`.
+6. Make sure to unset any aliases that take the name `pip`, `python`, `python3`, etc.. (`unalias pip`, `unalias python`, `unalias python3`, etc..)
+7. Install the requirements: `pip install -r requirements.txt`. This'll take a bit. Make sure you got good internet.
+8. Run `pyside6-uic form.ui -o ui_form.py` to generate `ui_form.py`.
+
+You're done installing.
+
+## How to run
+
+`sudo .venv/bin/python app.py`
+
+Lost needs root to run. Don't run it without `sudo`.
+
+## How to update
+
+Just download the source code of the [latest release](https://github.com/Butterroach/lost/releases/latest), extract into a new dir, and move the .venv folder from the old dir to the new one.
+
+You can delete the old dir after confirming the new version works.
+
+## How to contribute
+
+Fork the repo and make your changes! (Make sure you can Git and code Python)
+
+For changing the UI: Use Design mode in QT Creator (open the form.ui file).
+
+For changing the code: QT Creator is terrible for Python. Use VS Code instead of QT Creator.
+
+After doing whatever you did, open a pull request with a detailed changelog of what you did. I'll review it.
+
+Btw, PLEASE have clear commit messages!!!!
+
+## License
+
+Lost is licensed under the GNU GPL v3, or (at your option) any later version. See the LICENSE file for more info.
---
+++ app.py
@@ -0,0 +1,361 @@
+"""
+Lost: a hosts file manager for Linux
+
+Copyright (C) 2025 Butterroach
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import ipaddress
+import os
+import platform
+import re
+import requests
+import sys
+from packaging.version import Version
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QDialog,
+    QVBoxLayout,
+    QLabel,
+    QMessageBox,
+)
+from PySide6.QtCore import Qt, QUrl, QStringListModel
+from urllib.parse import urlparse
+from typing import Optional
+
+
+from ui_form import Ui_App  # generate ui_form.py: pyside6-uic form.ui -o ui_form.py
+
+# do NOT remove the above comment
+
+__version__ = "1.0.0"
+
+
+def isValidIP(ip):
+    ip = ip.split("#")[0].strip()
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+
+def isValidHostname(hostname):
+    hostname = hostname.split("#")[0].strip()
+
+    parsed = urlparse(f"//{hostname}")
+
+    if not parsed.hostname:
+        return False
+
+    if parsed.path or parsed.params or parsed.query or parsed.fragment:
+        return False
+
+    return parsed.hostname == hostname
+
+
+def validateHostsFile(data):
+    lines = data.splitlines()
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith("#") or not line:
+            continue
+
+        parts = line.split()
+
+        if len(parts) < 2:
+            return False
+
+        ip = parts[0]
+        if not isValidIP(ip):
+            return False
+
+        for hostname in parts[1:]:
+            if not isValidHostname(hostname):
+                return False
+
+    return True
+
+
+class HtmlWindow(QDialog):
+    def __init__(self, html_content, title, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle(title)
+        layout = QVBoxLayout()
+
+        label = QLabel(self)
+        label.setTextFormat(Qt.RichText)
+        label.setText(html_content)
+        label.setOpenExternalLinks(True)
+
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+
+class App(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        if platform.system() == "Windows" or "--pretend-like-im-windows" in sys.argv:
+            QMessageBox.critical(
+                self,
+                "Windows is unsupported",
+                "You are using Windows, which is NOT supported by Lost! While Lost can technically support Windows with a few tiny modifications, I just don't wanna do it. You have a million alternatives, pick one.",
+            )
+            sys.exit(127)
+
+        self.unsavedChanges = False
+
+        self.HOSTS_FILE = "/etc/hosts"  # * PLEASE set this to a file called "lost-test-hosts" somewhere that doesn't require root to access during development and testing PLEASEE
+        self.HOSTS_SEPARATOR = "\n# ENTRIES MADE BY LOST START HERE, ADD CUSTOM ENTRIES ABOVE AND DO NOT EDIT THE BELOW\n"
+        self.LOSTS_SEPARATOR = (
+            r"(# LOST URL https?://\S+ 192919291222//\./\./\./\./\.)\n"
+        )
+
+        if os.geteuid() != 0 and self.HOSTS_FILE.split("/")[-1] != "lost-test-hosts":
+            QMessageBox.critical(
+                self,
+                "Root required",
+                "Lost requires root to run! I tried implementing some pkexec stuff for this but that didn't work out well, so...",
+            )
+            QMessageBox.warning(
+                self,
+                "Quick warning",
+                "You're gonna be blinded by light mode QT if you run this in root lol (unless you wanna maybe set dark mode in root settings?)",
+            )
+            sys.exit(127)
+
+        with open(self.HOSTS_FILE, "r") as f:
+            self.hostsFile = f.read()
+
+        self.hostsFileParts = self.hostsFile.split(self.HOSTS_SEPARATOR)
+
+        if len(self.hostsFileParts) == 1:
+            if "\n# LOST URL" in self.hostsFileParts[0]:
+                QMessageBox.critical(
+                    self,
+                    "...",
+                    "Did you REALLY remove that warning? Why would you do that?",
+                )
+                sys.exit(127)
+            self.losts = []
+        elif self.hostsFileParts[1].strip().replace("\n", "").replace("\r", "") == "":
+            # the warning is there, but there are no losts
+            self.losts = []
+        elif len(self.hostsFileParts) > 2:
+            QMessageBox.critical(
+                self,
+                "?????",
+                "Can you NOT tamper with your hosts file like that???????",
+            )
+            sys.exit(127)
+        else:
+            self.losts = re.split(self.LOSTS_SEPARATOR, self.hostsFileParts[1])
+            if not self.losts[0]:
+                self.losts.pop(0)
+
+        try:
+            release_data = requests.get(
+                "https://api.github.com/repos/Butterroach/lost/releases"
+            ).json()
+            latest_ver = Version(release_data["tag_name"])
+            current_ver = Version(__version__)
+            if current_ver < latest_ver:
+                QMessageBox.information(
+                    self,
+                    "Update available",
+                    f"An update is available! You are on {__version__}, the latest version is {latest_ver}. Please update!",
+                )
+        except:
+            pass
+
+        self.ui = Ui_App()
+        self.ui.setupUi(self)
+
+        self.aboutDialog = HtmlWindow(
+            f'<DOCTYPE html!><html><body><h1>Lost {__version__}</h1><p><strong>Lost</strong> is a hosts file manager made for Linux.</p><p><a href="https://github.com/Butterroach/lost">Source code</a></p></body></html>',
+            "About",
+        )
+        self.ui.actionExit.triggered.connect(app.exit)
+        self.ui.actionAbout.triggered.connect(self.aboutDialog.show)
+        self.ui.lineEdit.returnPressed.connect(self.addSource)
+
+        self.model = QStringListModel()
+        self.ui.listView.setModel(self.model)
+
+        self.populateListView()
+
+        self.ui.saveButton.clicked.connect(self.saveChanges)
+        self.ui.removeButton.clicked.connect(self.removeSource)
+        self.ui.updateButton.clicked.connect(self.updateSource)
+        self.ui.updateAllButton.clicked.connect(self.updateAllSources)
+
+    def getSelectedURL(self) -> Optional[str]:
+        model = self.ui.listView.selectionModel()
+        indexes = model.selectedIndexes()
+
+        if indexes:
+            data = self.ui.listView.model().data(indexes[0])
+            if data:
+                return data
+        QMessageBox.critical(self, "No URL selected", "You need to select a URL!")
+
+    def addSource(self):
+        url = self.ui.lineEdit.text()
+        for i in range(0, len(self.losts), 2):
+            if url in self.losts[i]:
+                QMessageBox.critical(
+                    self,
+                    "URL already exists",
+                    "That URL already exists in the hosts file!",
+                )
+                return
+        if not QUrl(url).isValid():
+            QMessageBox.critical(self, "Invalid URL", "You entered an invalid URL.")
+            return
+        contents = requests.get(url).text
+        if not validateHostsFile(contents):
+            QMessageBox.critical(
+                self,
+                "Invalid hosts file",
+                "The contents of that hosts file is NOT valid!",
+            )
+            return
+        self.losts.append(f"# LOST URL {url} 192919291222//././././.")
+        self.losts.append(contents)
+        self.populateListView()
+        self.ui.lineEdit.clear()
+        self.unsavedChanges = True
+
+    def updateSource(self, url: str = None):
+        notUpdateAll = False
+        if (
+            isinstance(url, bool) or url is None
+        ):  # qt gives this function False as a parameter for whatever reason...
+            notUpdateAll = True
+            url = self.getSelectedURL()
+            if url is None:
+                return
+
+        for i in range(0, len(self.losts), 2):
+            if self.losts[i] == f"# LOST URL {url} 192919291222//././././.":
+                contents = requests.get(url).text
+                if contents.strip().replace("\n", "").replace("\r", "") == self.losts[
+                    i + 1
+                ].strip().replace("\n", "").replace("\r", ""):
+                    if notUpdateAll:
+                        QMessageBox.information(
+                            self,
+                            "Nothing to update",
+                            "There was nothing to update. If you are SURE that there is an update, try flushing your cache.",
+                        )
+                    return
+                if not validateHostsFile(contents):
+                    if notUpdateAll:
+                        QMessageBox.critical(
+                            self,
+                            "Invalid hosts file",
+                            "The contents of the hosts file that you tried to update has now become invalid. You will need to stay on the older version. Please contact the hosts file maintainer about this.",
+                        )
+                    else:
+                        QMessageBox.critical(
+                            self,
+                            "Invalid hosts file",
+                            f"One of your hosts files has become invalid: {url}. You will need to stay on the older version of that hosts file. Please contact the hosts file maintainer about this. Update will continue with the other hosts files after you click OK or close this message box.",
+                        )
+                    return
+                self.losts[i + 1] = contents
+                self.populateListView()
+                self.unsavedChanges = True
+                if notUpdateAll:
+                    QMessageBox.information(self, "Update done", "Done updating!")
+                return
+        QMessageBox.warning(
+            self,
+            "...What",
+            "Idk how to explain this just urgently open a github issue now the url provided to update wasn't found",
+        )
+
+    def updateAllSources(self):
+        for i in range(0, len(self.losts), 2):
+            self.updateSource(self.losts[i].split(" ")[3])
+        QMessageBox.information(self, "Update done", "Done updating!")
+
+    def removeSource(self):
+        url = self.getSelectedURL()
+        if url is None:
+            return
+        for i in range(0, len(self.losts), 2):
+            if url in self.losts[i]:
+                self.losts.pop(i)
+                self.losts.pop(i)
+                self.populateListView()
+                self.unsavedChanges = True
+                return
+
+    def populateListView(self):
+        urls = []
+        for i in range(0, len(self.losts), 2):
+            url_comment = self.losts[i]
+            match = re.search(
+                r"# LOST URL (https?://\S+) 192919291222//\./\./\./\./\.", url_comment
+            )
+            if match:
+                urls.append(match.group(1))
+        self.model.setStringList(urls)
+
+    def saveChanges(self):
+        new_hosts_file = self.hostsFileParts[0] + self.HOSTS_SEPARATOR
+        for i in range(0, len(self.losts), 2):
+            url_comment = self.losts[i]
+            domain_data = self.losts[i + 1]
+            new_hosts_file += url_comment + "\n" + domain_data + "\n"
+
+        with open(self.HOSTS_FILE, "w") as f:
+            f.write(new_hosts_file)
+
+        self.unsavedChanges = False
+
+        QMessageBox.information(
+            self, "Saved", "Changes have been saved to the hosts file!"
+        )
+
+    def closeEvent(self, event):
+        if self.unsavedChanges:
+            response = QMessageBox.question(
+                self,
+                "Unsaved changes!!",
+                "You have unsaved changes! Do you want to save them?",
+                QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel,
+                QMessageBox.Save,
+            )
+            if response == QMessageBox.Save:
+                self.saveChanges()
+                event.accept()  # exit
+            elif response == QMessageBox.Discard:
+                event.accept()  # exit
+            else:
+                event.ignore()  # user clicked cancel or closed the message box -- don't exit
+        else:
+            event.accept()  # exit
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = App()
+    widget.show()
+    sys.exit(app.exec())
